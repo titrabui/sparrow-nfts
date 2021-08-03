4f3f08ffd833260314391107ce82dfdb8fe6ada5
-g <linhnguyendeveloper@gmail.com>
2021-08-03 22:27:25 +0700

    Set up web3 front end

--- frontend/src/ui/Header/RightHeader.tsx
+++ frontend/src/ui/Header/RightHeader.tsx
@@ -1,27 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { Avatar } from 'antd';
 import DropdownMenu from './DropdownMenu';
 import { UserOutlined } from '@ant-design/icons';
 import styled from 'styled-components';
-import useWallet from 'hooks/useWallet';
 import Button from 'ui/Button';
+import useWallet from 'hooks/useWallet';
+import { getContract } from 'utils/getContract';
+import Web3 from 'web3';
 
 interface RightHeaderProps {}
 
 const RightHeader: React.FC<RightHeaderProps> = () => {
-  const { connect, active } = useWallet();
+  const { connect, active, connector, account } = useWallet();
 
   const handleConnectWallet = async (): Promise<any> => {
-    console.log('cpp');
-    
     await connect();
   };
+
+  const callSC = async (): Promise<any> => {
+    const contract = await getContract(connector);
+    const txHash = await contract.methods.allInitialOwnersAssigned().send({ from: account });
+  };
+
   return (
     <RightContainer>
-      <Button onClick={handleConnectWallet}>
-          Connect Wallet
-        </Button>
+      {!active && <Button onClick={handleConnectWallet}>Connect Wallet</Button>}
+      {active && <Button onClick={callSC}>Call SC</Button>}
+
       <DropdownMenu>
         <AvatarContainer icon={<UserOutlined />} />
       </DropdownMenu>
--- frontend/src/utils/getContract.ts
+++ frontend/src/utils/getContract.ts
@@ -1,19 +1,18 @@
-// /* eslint-disable import/prefer-default-export */
-// import Web3 from 'web3';
+/* eslint-disable import/prefer-default-export */
+import Web3 from 'web3';
 
-// import contract from 'utils/Contract.json';
+import contract from 'utils/contract.json';
 
-// export const getContract = async (connector: any) => {
-//   if (!connector) throw Error('No connector found');
-//   const walletProvider = await connector.getProvider();
-//   const web3 = new Web3(walletProvider);
-//   const contractAbi: any = contract.abi;
+export const getContract = async (connector: any) => {
+  if (!connector) throw Error('No connector found');
+  const walletProvider = await connector.getProvider();
+  const web3 = new Web3(walletProvider);
+  const contractAbi: any = contract.abi;
 
-//   // TODO: Get network address whenever JSON file change
-//   // const contractAddress = contract.networks[1622807330980].address;
-//   const contractAddress = contract.networks[5777].address;
+  // TODO: Get network address whenever JSON file change
+  // const contractAddress = contract.networks[1622807330980].address;
+  const contractAddress = '0x5fbdb2315678afecb367f032d93f642f64180aa3'
 
-//   return new web3.eth.Contract(contractAbi, contractAddress);
-// };
+  return new web3.eth.Contract(contractAbi, contractAddress);
+};
 
-export const getContract = () => {};
